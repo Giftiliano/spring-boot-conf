d1d5c9cfb22606149ed4a1264fea0c664b07026a
Giftiliano <giftomoigui4@gmail.com>
2025-01-17 21:19:06 +0100

    commit initial

---
+++ src/main/java/com/example/springbootconf/SpringBootConfApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfApplication.class, args);
+    }
+
+}
